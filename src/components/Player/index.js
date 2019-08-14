598aa13119f9459209c8c8e08dc185a386ce1e5f
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-14 18:02:01 -0300

    Add styles to player component.

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Footer } from './styles';
 
 export default function Player({ name }) {
   return (
     <Container>
-      <h1>{name}</h1>
+      <Footer>
+        <h1>{name}</h1>
+      </Footer>
     </Container>
   );
 }
--- src/components/Player/styles.js
+++ src/components/Player/styles.js
@@ -2,7 +2,21 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 250px;
-  background: red;
   margin: 5px;
   flex-grow: 1;
+  background-color: #f4f4ff;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  /* TODO Verificar como fazer isso aqui ficar dinâmico
+  */
+  margin-top: 750px;
+  height: 50px;
+  h1 {
+    color: black;
+  }
 `;
